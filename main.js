3d912617009e5d6acea4b3647c1a65da626cf12e
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2021-02-11 13:06:27 +0100

    Completata milestone 2, 3 e in corso milestone 4

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 var app = new Vue({
   el : '#app',
   data: {
+    all : [],
     film : [],
-    search : ''
+    serie: [],
+    search : '',
+    languages : ['en', 'it', 'es', 'gr', 'cn', 'de', 'fr', 'ja']
   },
   methods: {
-    cerca : function(){
+    searchAll : function(){
+      this.all = [];
+      this.cercaFilm();
+      this.cercaSerie();
+    },
+    cercaFilm : function(){
       const self = this;
         axios.get('https://api.themoviedb.org/3/search/movie?api_key=fe5018503d4af1e28f0fda5806e5e4c8&query=' + self.search).
           then(function(resp){
             const films = resp.data.results;
             self.film = films;
+            self.all = [...self.all, ...self.film];
           })
       console.log(self.film);
+    },
+    cercaSerie : function(){
+      const self = this;
+          axios.get('https://api.themoviedb.org/3/search/tv?api_key=fe5018503d4af1e28f0fda5806e5e4c8&query=' + self.search).
+            then(function(resp){
+              const serie = resp.data.results;
+              self.serie = serie;
+              self.all = [...self.all, ...self.serie];
+            })
+      console.log(self.serie);
+    },
+    getVote: function(vote){
+      return parseInt(vote / 2);
     }
   }
 })
